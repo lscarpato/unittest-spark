d7ee4a5d02783d90bffa43f7a2c6bbf916204319
lscarpato <luciana.scarpato@gmail.com>
2016-08-03 08:29:52 -0300

    initial commit

--- tests/tests.py
+++ tests/tests.py
@@ -44,28 +44,9 @@ class Testsitapl02(unittest2.TestCase):
     
     def test_such_as_original_code(self):
     
-        rows = [Row('$1$BXSWV24w$n3Fk0n9UZMDrMy80gXALP.-760-3-3', 1.0),
-                Row('$1$6XfmeOOM$TgISMVWjJ4HlAScJhtFyq/-2911-3-3', 1.0), 
-                Row('$1$6XfmeOOM$TgISMVWjJ4HlAScJhtFyq/-2530-3-3', 1.0)]
-        file_train = "../resources/sitapl02_saldo_conta_train.csv"
-        self.call_modelST2(rows, file_train)        
+             
         
-    def test_model_validation(self):
-        #somente positivos        
-        rows = [Row('$1$ryh3v7.g$IlBuGfBDrcOCU2bBXmv751-1298-3-3', 1.0),
-                Row('$1$o9kaQ5ac$13Tpv7/qtOMa2ldDqKSKM/-5302-3-3', 0.87)]
-        file_test = "../resources/sitapl02_saldo_conta_test.csv"
-        self.call_modelST2(rows, file_test)
-        
-    def call_modelST2( self, rows, file_data ):
-        rdd = self.sc.parallelize(rows)
-        schema = StructType([StructField('account_num', StringType(), True), StructField('percentil', DoubleType(), True)])
-        df_resultExpected = self.sqlContext.createDataFrame(self.sqlContext.createDataFrame(rdd).take(10), schema)
-        df_dataSet = self.sqlContext.read.format('com.databricks.spark.csv').options(header='true', delimiter=',').load(file_data)
-        
-        df_result = application.modelST2(df_dataSet, 10, 0).toDF()
-        df_resultExpected.sort(df_resultExpected.account_num.asc())
-        df_result.sort(df_result.account_num.asc())
+
         
     def quiet_logs( self, sc ):
       logger = sc._jvm.org.apache.log4j
